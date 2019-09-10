1a040108a89a9a7b79fca046cb59697a40204dd7
Woncheol Heo <>
2019-09-10 18:15:42 +0900

    sendClick, sendGoal visitNew 'Y' 중복현상 해결

--- WiseTracker.framework/Headers/WiseTrackerCore.h
+++ WiseTracker.framework/Headers/WiseTrackerCore.h
@@ -37,7 +37,7 @@ typedef uint_least32_t char32_t;
 typedef struct _NSZone NSZone;
 
 //WiseTracker Version
-#define WISETRACKER_SDK_VERSION @"21.3.12"
+#define WISETRACKER_SDK_VERSION @"21.3.13"
 
 @interface WiseTrackerCore : NSObject
 + (UIApplication *)application;
