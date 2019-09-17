4a91076a55a08a01587bf10a5fc0eeeca66fe0fa
Woncheol Heo <>
2019-09-17 18:57:04 +0900

    iOS 13 대응 코드 추가

--- WiseTracker.framework/Headers/WiseTrackerCore.h
+++ WiseTracker.framework/Headers/WiseTrackerCore.h
@@ -37,7 +37,7 @@ typedef uint_least32_t char32_t;
 typedef struct _NSZone NSZone;
 
 //WiseTracker Version
-#define WISETRACKER_SDK_VERSION @"21.3.13"
+#define WISETRACKER_SDK_VERSION @"21.3.14"
 
 @interface WiseTrackerCore : NSObject
 + (UIApplication *)application;
