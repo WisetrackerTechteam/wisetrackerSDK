2ae2694cbfa7afc2c71da3bb09c3990db25eca83
Woncheol Heo <>
2019-09-30 11:47:25 +0900

    버전 업데이트

--- WiseTracker.framework/Headers/WiseTrackerCore.h
+++ WiseTracker.framework/Headers/WiseTrackerCore.h
@@ -37,7 +37,7 @@ typedef uint_least32_t char32_t;
 typedef struct _NSZone NSZone;
 
 //WiseTracker Version
-#define WISETRACKER_SDK_VERSION @"21.3.15"
+#define WISETRACKER_SDK_VERSION @"21.3.16"
 
 @interface WiseTrackerCore : NSObject
 + (UIApplication *)application;
